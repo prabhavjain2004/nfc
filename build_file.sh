855a3fa60e82a5773f25ce008552cdf00022f949
prabhavjain2004 <aadimate2004@gmail.com>
2025-03-28 14:49:52 +0530

    g

--- build_file.sh
+++ build_file.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+# Set Python path
+export PATH="/opt/conda/bin:$PATH"
+
+echo "Python version:"
+python --version
+
 echo "Installing dependencies..."
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 # Skip migrations during build as they should be handled during development
 # python manage.py makemigrations
